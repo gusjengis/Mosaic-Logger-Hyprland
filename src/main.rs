46861c66b1ee6d09ce220d72acf9c5934c0ecbad
gusjengis <anthony.j.green@outlook.com>
2025-03-27 22:55:08 -0700

    basic event listener, prints active window data when changing focus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use hyprland::{async_closure, event_listener::AsyncEventListener};
+
+#[tokio::main]
+async fn main() -> hyprland::Result<()> {
+    let mut event_listener = AsyncEventListener::new();
+
+    event_listener.add_active_window_changed_handler(
+        async_closure! { move |data| println!("window changed to {data:#?}")},
+    );
+
+    event_listener.start_listener_async().await
 }
