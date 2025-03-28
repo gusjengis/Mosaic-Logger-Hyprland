f0151cc36c7ebf2fd0a52d474dd6fb8984dc93db
gusjengis <anthony.j.green@outlook.com>
2025-03-27 23:19:51 -0700

    Cleaned up event handler, added chrono, now printing the exact data I want to send to DB

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use hyprland::{async_closure, event_listener::AsyncEventListener};
 async fn main() -> hyprland::Result<()> {
     let mut event_listener = AsyncEventListener::new();
 
-    event_listener.add_active_window_changed_handler(
-        async_closure! { move |data| println!("window changed to {data:#?}")},
-    );
+    event_listener.add_active_window_changed_handler(async_closure! { move |window_data| {
+            if let Some(data) = window_data {
+                println!("{} - {}: {}", chrono::Local::now().timestamp_millis(), data.class, data.title);
+            }
+        }
+    });
 
     event_listener.start_listener_async().await
 }
